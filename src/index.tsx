561ea8f73d9a6be07ab20e8a3f9be31cd4aa3c5c
symysak <yoshiaki@the.suyamas.jp>
2023-02-28 17:53:45 +0900

    fix bug

--- src/index.tsx
+++ src/index.tsx
@@ -9,16 +9,7 @@ const titleTag="Network Tools - SUYAMA";
 root.render(
     <React.StrictMode>
         <HelmetProvider>
-            <head>
-                <meta charSet="utf-8" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&subset=japanese" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-                <title>{titleTag}</title>
-            </head>
-        </HelmetProvider>
-        <body>
             <App />
-        </body>
+        </HelmetProvider>
     </React.StrictMode>
 );
