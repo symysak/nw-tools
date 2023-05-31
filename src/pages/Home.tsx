86765772bb1b3df68e67c7e3206d6bfcf7947c89
symysak <yoshiaki@the.suyamas.jp>
2023-05-31 20:58:55 +0900

    Change Title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,12 @@ import { Helmet } from "react-helmet-async";
 import { Typography } from "@mui/material";
 
 function Home() {
-    const titleTag="ホーム";
     return (
         <div>
             <Helmet>
-                <title>{titleTag + " - Network Tools | SUYAMA"}</title>
+                <title>{"Network Tools | SUYAMA"}</title>
             </Helmet>
-            <Dashboard title={titleTag} >
+            <Dashboard title={"Network Tools"} >
                 <Typography color="inherit" variant="body2" component="p">
                     個人的に欲しいなと思ったネットワーク系のツールをいろいろ作ってまとめているサイト。
                 </Typography>
