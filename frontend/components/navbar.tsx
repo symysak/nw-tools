acad9aa69565a89c9cc0d527576972ccf9589ffe
Yama <103905928+symysak@users.noreply.github.com>
2023-11-05 22:41:48 +0900

    Update navbar.tsx

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -50,7 +50,7 @@ export const Navbar = () => {
 	);
 
 	return (
-		<NextUINavbar maxWidth="xl" position="sticky">
+		<NextUINavbar maxWidth="xl" position="sticky" shouldHideOnScroll>
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
