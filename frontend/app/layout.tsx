43620a43ed7422dab38e1ec76c004c23d61fa41b
Yama <103905928+symysak@users.noreply.github.com>
2023-11-05 22:39:20 +0900

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" suppressHydrationWarning>
+		<html lang="ja" suppressHydrationWarning>
 			<head />
 			<body
 				className={clsx(
